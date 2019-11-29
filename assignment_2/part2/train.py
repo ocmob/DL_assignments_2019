796d006965f47e03d134bdb85f3c400cb0258d30
Kacper Urbanski <lgpu0296@test3.irc.sara.nl.irc.sara.nl>
2019-11-29 10:37:34 +0100

    mods

--- assignment_2/part2/train.py
+++ assignment_2/part2/train.py
@@ -61,75 +61,75 @@ def train(config):
     # Setup the loss and optimizer
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.RMSprop(model.parameters(), config.learning_rate)
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=50, verbose=True)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=config.learning_rate_step, gamma=config.learning_rate_decay)
 
-    for i in range(100):
-        for step, (batch_inputs, batch_targets) in enumerate(data_loader):
+    for step, (batch_inputs, batch_targets) in enumerate(data_loader):
 
-            model.zero_grad()
+        model.zero_grad()
 
 
-            # Only for time measurement of step through network
-            t1 = time.time()
+        # Only for time measurement of step through network
+        t1 = time.time()
 
 #            batch_inputs = F.one_hot(batch_inputs, num_classes=dataset.vocab_size,
 #                    ).view(30, 64, -1).float().to(device)
-            batch_inputs = F.one_hot(batch_inputs, num_classes=dataset.vocab_size,
-                    ).float().to(device)
-            #batch_inputs = batch_inputs.float().view(config.seq_length, -1, 1).float().to(device)
+        batch_inputs = F.one_hot(batch_inputs, num_classes=dataset.vocab_size,
+                ).float().to(device)
+        #batch_inputs = batch_inputs.float().view(config.seq_length, -1, 1).float().to(device)
 
-            batch_targets = batch_targets.to(device)
+        batch_targets = batch_targets.to(device)
 
-            optimizer.zero_grad()
-            pred = model.forward(batch_inputs)
-            #SHOW THIS
-            #loss = criterion(pred.view(-1, 84, 30), batch_targets)
+        optimizer.zero_grad()
+        pred = model.forward(batch_inputs)
+        #SHOW THIS
+        #loss = criterion(pred.view(-1, 84, 30), batch_targets)
 
-            loss = criterion(pred.transpose(2, 1), batch_targets)
-            accuracy = acc(pred.transpose(2, 1), F.one_hot(batch_targets, num_classes=dataset.vocab_size).float(), dataset.vocab_size) 
+        loss = criterion(pred.transpose(2, 1), batch_targets)
+        accuracy = acc(pred.transpose(2, 1), F.one_hot(batch_targets, num_classes=dataset.vocab_size).float(), dataset.vocab_size) 
 
-            #SHOW THIS
-            #accuracy = acc(pred.view(-1, 84, 30), F.one_hot(batch_targets, num_classes=dataset.vocab_size).float(), dataset.vocab_size) 
-            loss.backward()
-            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=config.max_norm)
-            optimizer.step()
+        #SHOW THIS
+        #accuracy = acc(pred.view(-1, 84, 30), F.one_hot(batch_targets, num_classes=dataset.vocab_size).float(), dataset.vocab_size) 
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=config.max_norm)
+        optimizer.step()
 
-            # Just for time measurement
-            t2 = time.time()
-            examples_per_second = config.batch_size/float(t2-t1)
+        # Just for time measurement
+        t2 = time.time()
+        examples_per_second = config.batch_size/float(t2-t1)
 
-            if step % config.print_every == 0:
-                scheduler.step(loss)
-                print("[{}] Train Step {:04d}/{:04d}, Batch Size = {}, Examples/Sec = {:.2f}, "
-                      "Accuracy = {:.2f}, Loss = {:.3f}".format(
-                        datetime.now().strftime("%Y-%m-%d %H:%M"), step,
-                        config.train_steps, config.batch_size, examples_per_second,
-                        accuracy, loss
-                ))
+        scheduler.step()
 
-            if (step % config.sample_every) == 0:
-                with torch.no_grad():
-                    codes = []
+        if step % config.print_every == 0:
+            print("[{}] Train Step {:04d}/{:04d}, Batch Size = {}, Examples/Sec = {:.2f}, "
+                  "Accuracy = {:.2f}, Loss = {:.3f}".format(
+                    datetime.now().strftime("%Y-%m-%d %H:%M"), step,
+                    config.train_steps, config.batch_size, examples_per_second,
+                    accuracy, loss
+            ))
 
-                    input_tensor = torch.zeros((1, 1, dataset.vocab_size), device=device)
-                    input_tensor[0, 0, np.random.randint(0, dataset.vocab_size)] = 1
+        if (step % config.sample_every) == 0:
+            with torch.no_grad():
+                codes = []
 
-                    for i in range(config.seq_length-1):
-                        response = model.step(input_tensor)
-                        logits = F.log_softmax(config.temp*response, dim=1)
-                        dist = torch.distributions.one_hot_categorical.OneHotCategorical(logits=logits)
-                        code = dist.sample().argmax().item()
-                        input_tensor *= 0
-                        input_tensor[0, 0, code] = 1
-                        codes.append(code)
-                    string = dataset.convert_to_string(codes)
-                    model.reset_stepper()
-                    print(string)
+                input_tensor = torch.zeros((1, 1, dataset.vocab_size), device=device)
+                input_tensor[0, 0, np.random.randint(0, dataset.vocab_size)] = 1
 
-            if step == config.train_steps:
-                # If you receive a PyTorch data-loader error, check this bug report:
-                # https://github.com/pytorch/pytorch/pull/9655
-                break
+                for i in range(config.seq_length-1):
+                    response = model.step(input_tensor)
+                    logits = F.log_softmax(config.temp*response, dim=1)
+                    dist = torch.distributions.one_hot_categorical.OneHotCategorical(logits=logits)
+                    code = dist.sample().argmax().item()
+                    input_tensor *= 0
+                    input_tensor[0, 0, code] = 1
+                    codes.append(code)
+                string = dataset.convert_to_string(codes)
+                model.reset_stepper()
+                print(string)
+
+        if step == config.train_steps:
+            # If you receive a PyTorch data-loader error, check this bug report:
+            # https://github.com/pytorch/pytorch/pull/9655
+            break
 
     print('Done training.')
 
